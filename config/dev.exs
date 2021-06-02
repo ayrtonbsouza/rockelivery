2b041d1b8e22adab745b4989f1decaddba761c48
Ayrton Souza <ayrtonsouza@icloud.com>
2021-06-02 12:17:12 -0300

    :hammer_and_wrench: : database configuration

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,11 @@ use Mix.Config
 
 # Configure your database
 config :rockelivery, Rockelivery.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "user",
+  password: "password",
   database: "rockelivery_dev",
   hostname: "localhost",
+  port: 5432,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
--- config/test.exs
+++ config/test.exs
@@ -6,10 +6,11 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :rockelivery, Rockelivery.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "user",
+  password: "password",
   database: "rockelivery_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
+  port: 5432,
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
