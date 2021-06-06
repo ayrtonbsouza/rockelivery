f88da5d73a7658206786df75159d5fc48b44a4c9
Ayrton Souza <ayrtonsouza@icloud.com>
2021-06-06 02:07:38 -0300

    :sparkles: : show users route development

--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -4,13 +4,21 @@ defmodule RockeliveryWeb.UsersController do
   alias Rockelivery.User
   alias RockeliveryWeb.FallbackController
 
-  action_fallback FallbackController
+  action_fallback(FallbackController)
 
   def create(connection, params) do
     with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
       connection
       |> put_status(:created)
       |> render("create.json", user: user)
+    end
+  end
+
+  def show(connection, %{"id" => id}) do
+    with {:ok, %User{} = user} <- Rockelivery.get_user_by_id(id) do
+      connection
+      |> put_status(:ok)
+      |> render("show.json", user: user)
     end
   end
 end
